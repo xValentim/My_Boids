a01d4f0b8d58bb6601080e55617c39199d0108ea
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-04-16 13:31:23 -0300

    Introduzir outras dinamicas daqui.

---
+++ explosion_v0/boid copy.py
@@ -0,0 +1,65 @@
+import pygame
+import random
+import math
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def add_vector(self, u):
+        self.x += u.x
+        self.y += u.y
+
+    def sub_vector(self, u):
+        self.x -= u.x
+        self.y -= u.y
+
+    def set_magnitude(self, k):
+        self.x = (self.x) * k
+        self.y = (self.y) * k
+
+    def createRandom2D(self):
+        self.x = random.uniform(-1, 1)
+        self.y = random.uniform(-1, 1)
+    
+    def createRandom2D_mag1(self):
+        teta = random.random() * 360
+        teta = math.radians(teta)
+        x = math.sin(teta)
+        y = math.cos(teta)
+        self.x = x
+        self.y = y
+
+class Boid:
+    def __init__(self, x0, y0, vx0, vy0, color=(255, 255, 255)):
+        self.position = Vector(x0, y0)
+        self.velocity = Vector(vx0, vy0)
+        self.acceleration = Vector(0, 0)
+        self.color = color
+    
+    def update(self):
+        self.position.add_vector(self.velocity)
+        self.velocity.add_vector(self.acceleration)
+
+    #### Craig Reynolds model ####
+    # steer to avoid crowding local flockmates
+    def separation(self):
+        pass
+        #TODO
+
+    # steer towards the average heading of local flockmates
+    def align(self):
+        pass
+        #TODO
+    
+    # steer to move toward the average position of local flockmates
+    def cohesion(self):
+        pass
+        #TODO
+
+    def show(self): 
+        #TODO
+        #pygame.draw.circle(window, red, (p2.x, p2.y), p2.r)
+        pass
+        
---
+++ explosion_v0/main copy.py
@@ -0,0 +1,47 @@
+import pygame
+import numpy
+import math
+import random
+from boid import *
+
+pygame.init()
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+gray = (35, 35, 35)
+largura = 640
+altura = 300
+r = 5
+
+flock = []
+for i in range(100):
+    b = Boid(largura / 2, altura / 2, 0, 0)
+    b.velocity.createRandom2D()
+    flock.append(b)
+
+relogio = pygame.time.Clock()
+window = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Boids")
+window.fill(gray)
+
+continua = True
+while continua:
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            continua = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                continua = False
+    
+    window.fill(gray)
+    for b0 in flock:
+        b0.update()
+    for b0 in flock:
+        pygame.draw.circle(window, b0.color, (b0.position.x, b0.position.y), r)
+    relogio.tick(60)
+    pygame.display.update()
+
+pygame.quit()
