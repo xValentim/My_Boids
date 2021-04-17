c3ed2660df252d21dea1c95eb20ad5f65ac4181c
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-04-17 13:35:23 -0300

    Separação dos valores constantes

---
+++ values.py
@@ -0,0 +1,14 @@
+# Cores
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+gray = (35, 35, 35)
+
+# Dimensões
+largura = 640
+altura = 300
+
+# Valor de criação e aniquilação
+k = 10
